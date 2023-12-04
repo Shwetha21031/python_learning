5e7c4f1c01ed597c871658abc85752fecb74882d
Shwetha21031 <Shwetha21031@gmail.com>
2023-12-04 09:40:37 +0530

    commit

--- FAST_API/SQL/Demo/demo.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine , ForeignKey , String , Integer , Column
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
-# declarative base = we are going to use this to create the model of our table ( class )
-Base = declarative_base()
-
